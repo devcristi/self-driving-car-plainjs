67b8aaad815d3d005cc1b8238602f5b303bd21d7
devcristi <121441678+devcristi@users.noreply.github.com>
2023-09-25 19:07:16 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+//defining the road width an height with js
+const canvas = document.getElementById("myCanvas");
+
+canvas.width = window.innerWidth;
+
+canvas.width=200;
+
+//drawing the car in 2d
+
+const ctx = canvas.getContext("2d");
+const car = new Car(100,100,30,50);
+
+car.draw(ctx);
+
+animate();
+
+function animate()
+{
+    car.update();
+    canvas.height = window.innerHeight;
+    car.draw(ctx);
+    requestAnimationFrame(animate);
+}
